28761eadf97f10a9aaec37a49b3be2af69e65815
Euphie Zhao <euphiezyf@gmail.com>
2021-02-25 20:11:31 -0800

    update

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'A7QSqdCabqd5V2G7Ghjk2krzsZiIdwLR';
+
+	// 'map' refers to a <div> element with the ID map
+	L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	// add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
